d881a83488c829b93f7ec9be8bdf5a268c48bb47
Alexandre Henrique Nolla <alexandre.nolla@gmail.com>
2023-11-27 23:43:03 -0300

    feat(logger): logger para debug na conexao com db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,40 @@
-import { Module } from '@nestjs/common';
+import { Logger, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsuariosModule } from './usuarios/usuarios.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    UsuariosModule,
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot(
+      {
+        type: process.env.DB_TYPE as any,
+        host: 'db',
+        port: parseInt(process.env.PG_PORT),
+        username: process.env.PG_USER,
+        password: process.env.PG_PASSWORD,
+        database: process.env.PG_DB,
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        synchronize: true, // Cria e atualiza as tabelas postgres automaticamente
+      }
+    )],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  constructor() {
+    // Logger para debugar a conexão com o db
+    const logger = new Logger('TypeORM Configuration');
+    logger.log({
+      type: process.env.DB_TYPE as any,
+      host: process.env.PG_HOST,
+      port: parseInt(process.env.PG_PORT),
+      username: process.env.PG_USER,
+      password: process.env.PG_PASSWORD,
+      database: process.env.PG_DB,
+    });
+  }
+}
