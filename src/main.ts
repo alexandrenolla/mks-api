f279254297b7c8ed53aac401e72c60ac8c1bd48e
Alexandre Henrique Nolla <alexandre.nolla@gmail.com>
2023-11-29 16:08:17 -0300

    feat(config): adiciona global-validation-pipe

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { logger: ['error', 'warn', 'log', 'debug'] });
+  app.useGlobalPipes(new ValidationPipe()); // Validadores 
   await app.listen(3000);
 }
 bootstrap();
